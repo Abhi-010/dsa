af86fba52a00f7a08ada4c8fba5f511f54300217
Abhishek <abhishek.km010@gmail.com>
2024-11-26 13:58:38 +0530

    bubble sort

--- src/main/java/sorting/Client.java
+++ src/main/java/sorting/Client.java
@@ -2,19 +2,34 @@ package sorting;
 
 public class Client {
     private static SortingAlgorithm sortingAlgorithm = new SelectionSort() ;
+
     public static void main(String[] args) {
-        //int[] inputArray = new int[]{1,2,3,4,-5} ;
-        int[] inputArray = createInputRandomArray(2000) ;
-        int[] outputArray = sortingAlgorithm.sort(inputArray) ;
+
+        int[] inputArray = createInputRandomArray(20) ;
+        int[] outputArray = sortingAlgorithm.sort(new int[]{-4,10,0,1,-4,23,100,4,-4}) ;
+
 
         for(int i = 0 ; i < outputArray.length ; i++){
             System.out.print(outputArray[i] + " ");
         }
+        System.out.println();
+
+        sortingAlgorithm = new BubbleSort() ;
+
+        inputArray = createInputRandomArray(20);
+        outputArray = sortingAlgorithm.sort(new int[]{-4,10,0,1,-4,23,100,4,-4});
+
+        for(int i = 0 ; i < outputArray.length ; i++){
+            System.out.print(outputArray[i] + " ");
+        }
+
     }
     private static int[] createInputRandomArray(int numberOfElements){
+
         int[] array = new int[numberOfElements];
+
         for(int i = 0 ; i < numberOfElements ; i++){
-            array[i] = (int)(Math.random()*1000) ;
+            array[i] = -(int)(Math.random()*1000) ;
         }
         return array;
     }
